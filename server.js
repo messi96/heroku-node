600f0c30d0157cf736b2c71645d2d9233f678c15
Rajat Moury <messi96@users.noreply.github.com>
2020-04-08 00:11:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/poc', function(req, res) {
 
 	// ejs render automatically looks in the views folder
 	res.render('index');
